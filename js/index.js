c793368fc175c669e5afc7c87cc42b3cd39f3250
Orca <wbjung2917@gmail.com>
2022-10-18 10:59:36 +0900

    confirm base layout

--- js/index.js
+++ js/index.js
@@ -8,3 +8,16 @@ function clickClear(){
     history.innerHTML="";
     formula.innerHTML="";
 }
+function clickOperators(operator){    
+    let formula=document.getElementById("formula");
+    let operatorList=["+","-","*","÷"]
+    if(formula.innerHTML.trim()==""){
+        console.log("아무것도 없는곳에 연산자를 입력할 수 없습니다.")
+    }
+    else if(!operatorList.includes(formula.innerHTML.charAt(formula.innerHTML.length-1))){
+        formula.innerHTML+=operator.innerHTML;
+    }
+    else{
+        console.log("연산자를 이어서 입력할 수 없습니다.");
+    }
+}
